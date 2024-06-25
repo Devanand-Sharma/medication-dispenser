33f2e08b130826f9fd0fd8a4ad6b319e1066ca99
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-06-25 16:27:56 -0400

    Created Home Page
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,170 +1,90 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
-import 'package:draggable_home/draggable_home.dart';
+import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
+import 'package:medication_app/widgets/home_medication_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  static const appBarHeight = 115.0;
+  static const appBarSpacing = 5.0;
+
+  // Medical Summary Under App Bar
+  Widget _buildSummaryReport(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(0),
+      elevation: 2.0,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Theme.of(context).colorScheme.primary,
+        height: appBarHeight,
+        child: Column(
+          children: [
+            Text(
+              DateFormat('EEEE, MMMM dd, yyyy').format(DateTime.now()),
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const Gap(appBarSpacing),
+            Text(
+              '4 Medications',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const Gap(appBarSpacing),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Log All As Taken'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    ListView listView() {
-      // TODO: Replace with actual data, home page medications
-      return ListView.builder(
-        padding: const EdgeInsets.only(top: 0),
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: 20,
-        shrinkWrap: true,
-        itemBuilder: (context, index) => Card(
-          color: Colors.white70,
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Text("$index"),
-            ),
-            title: const Text("Title"),
-            subtitle: const Text("Subtitle"),
-          ),
-        ),
-      );
-    }
+    final List<int> numbers = List.generate(20, (index) => index);
 
-    // Row headerBottomBarWidget() {
-    //   return Row(
-    //     mainAxisSize: MainAxisSize.max,
-    //     mainAxisAlignment: MainAxisAlignment.end,
-    //     crossAxisAlignment: CrossAxisAlignment.center,
-    //     children: [
-    //       IconButton(
-    //         onPressed: () {},
-    //         icon: const Icon(
-    //           Icons.settings,
-    //           color: Colors.white,
-    //         ),
-    //       )
-    //     ],
-    //   );
-    // }
-
-    Widget headerWidget(BuildContext context) {
-      return Column(
-        children: [
-          SizedBox(height: MediaQuery.of(context).padding.top),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 85,
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                  onPressed: () {
-                    log('Settings Pressed');
-                  },
-                  child: Text('Settings',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                      )),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  log('Profile Pressed');
-                },
-                child: Row(
-                  children: [
-                    const Icon(
-                      Icons.account_circle,
-                      color: Colors.white,
-                      size: 50,
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      'Harambe',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: 85,
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    log('Logout Pressed');
-                  },
-                  child: Text('Logout',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                      )),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Text('Sunday, June 23, 2024',
-              style: TextStyle(
-                fontSize: 16,
-                color: Theme.of(context).colorScheme.onPrimary,
-              )),
-          const SizedBox(height: 5),
-          Text('4 Medications',
-              style: TextStyle(
-                fontSize: 16,
-                color: Theme.of(context).colorScheme.onPrimary,
-              )),
-          const SizedBox(height: 5),
-          ElevatedButton(
-            onPressed: () {
-              log('Log All as Taken Pressed');
-            },
-            child: const Text('Log All as Taken'),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.people),
+            onPressed: () {},
+          )
         ],
-      );
-    }
-
-    return DraggableHome(
-      expandedHeight: 170,
-      curvedBodyRadius: 5,
-      centerTitle: false,
-      title: GestureDetector(
-        onTap: () {
-          log('Profile Pressed');
-        },
-        child: Row(mainAxisSize: MainAxisSize.min, children: [
-          Icon(
-            Icons.account_circle,
-            color: Theme.of(context).colorScheme.onPrimary,
-            size: 50,
-          ),
-          const SizedBox(width: 8),
-          Text('Harambe',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
-              ))
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.account_circle,
+              size: 40,
+            ),
+            Gap(appBarSpacing),
+            Text(
+              'Harambe',
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        color: Colors.deepPurple.shade100,
+        child: Column(children: [
+          _buildSummaryReport(context),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(5),
+              shrinkWrap: true,
+              itemCount: numbers.length,
+              itemBuilder: (context, index) {
+                return const HomeMedicationCard();
+              },
+            ),
+          )
         ]),
       ),
-      actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 10.0),
-          child: ElevatedButton(
-            onPressed: () {
-              log('Take All Pressed');
-            },
-            child: const Text('Take All'),
-          ),
-        ),
-      ],
-      headerWidget: headerWidget(context),
-      body: [
-        listView(),
-      ],
-      fullyStretchable: false,
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      appBarColor: Theme.of(context).colorScheme.primary,
     );
   }
 }
