d2681f903c8bd5814e2eaa098bb2bdee1612f6f4
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-06-25 19:14:03 -0400

    Created History Page Appbar and Calendar
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
 
   @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  final CalendarFormat _calendarFormat = CalendarFormat.week;
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.calendar_month_rounded),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Ionicons.document_text),
+          )
+        ],
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            calendarStyle: CalendarStyle(
+              todayDecoration: const BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Colors.deepPurple.shade300,
+                shape: BoxShape.circle,
+              ),
+              selectedTextStyle: const TextStyle(color: Colors.white),
+              todayTextStyle: const TextStyle(color: Colors.black),
+            ),
+            weekendDays: const [],
+            calendarFormat: _calendarFormat,
+            availableCalendarFormats: const {
+              CalendarFormat.week: 'Week',
+            },
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            firstDay: DateTime.utc(2012, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          ),
+          const Divider(),
+        ],
+      ),
+    );
   }
 }
