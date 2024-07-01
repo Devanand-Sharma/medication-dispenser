968cc9a67f6184f492112131e1072007f73ccad2
Devanand Sharma <devanandinquiries@gmail.com>
2024-06-30 21:01:56 -0400

    Create medication page

--- lib/screens/medication.dart
+++ lib/screens/medication.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:medication_app/widgets/medication_card.dart';
 
-class MedicationScreen extends StatelessWidget {
+class MedicationScreen extends StatefulWidget {
   const MedicationScreen({super.key});
 
   @override
+  State<MedicationScreen> createState() => _MedicationScreenState();
+}
+
+class _MedicationScreenState extends State<MedicationScreen> {
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Ionicons.search),
+          )
+        ],
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Container(
+        color: Colors.deepPurple.shade100,
+        child: Column(children: [
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(5),
+              shrinkWrap: true,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return const MedicationCard();
+              },
+            ),
+          )
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // TODO: Add a new page to be popped up when button is clicked.
+        },
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 }
