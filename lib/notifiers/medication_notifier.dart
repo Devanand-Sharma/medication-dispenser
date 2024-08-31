c7a8c1d33f0a0c861881205d25608034df2b9834
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-08-31 17:24:34 -0400

    Added Snackbars to Show CRUD Response Status
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- lib/notifiers/medication_notifier.dart
+++ lib/notifiers/medication_notifier.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:medication_app/database_manager/hive_manager.dart';
 import 'package:medication_app/database_manager/models/medication.dart';
+import 'package:medication_app/models/operation_status.dart';
 
 class MedicationNotifier extends StateNotifier<List<Medication>> {
   MedicationNotifier() : super([]) {
@@ -18,23 +19,50 @@ class MedicationNotifier extends StateNotifier<List<Medication>> {
     return await HiveManager.getLength<Medication>(BoxNames.medications);
   }
 
-  Future<void> addMedication(Medication medication) async {
-    state = [...state, medication];
-    await HiveManager.add<Medication>(BoxNames.medications, medication);
+  Future<OperationStatus> addMedication(Medication medication) async {
+    try {
+      state = [...state, medication];
+      await HiveManager.add<Medication>(BoxNames.medications, medication);
+      return OperationStatus.success;
+    } catch (e) {
+      print('Error adding medication: $e');
+      return OperationStatus.failure;
+    }
   }
 
-  Future<void> removeMedication(Medication medication) async {
-    state = state.where((e) => e.key != medication.key).toList();
-    await HiveManager.delete<Medication>(BoxNames.medications, medication.key);
+  Future<OperationStatus> removeMedication(Medication medication) async {
+    try {
+      state = state.where((e) => e.key != medication.key).toList();
+      await HiveManager.delete<Medication>(
+          BoxNames.medications, medication.key);
+      return OperationStatus.success;
+    } catch (e) {
+      print('Error removing medication: $e');
+      return OperationStatus.failure;
+    }
   }
 
-  Future<void> updateMedication(int key, Medication medication) async {
-    state = state.map((e) => e.key == key ? medication : e).toList();
-    await HiveManager.update<Medication>(BoxNames.medications, key, medication);
+  Future<OperationStatus> updateMedication(
+      int key, Medication medication) async {
+    try {
+      state = state.map((e) => e.key == key ? medication : e).toList();
+      await HiveManager.update<Medication>(
+          BoxNames.medications, key, medication);
+      return OperationStatus.success;
+    } catch (e) {
+      print('Error updating medication: $e');
+      return OperationStatus.failure;
+    }
   }
 
-  Future<void> clearMedications() async {
-    await HiveManager.clear<Medication>(BoxNames.medications);
-    state = [];
+  Future<OperationStatus> clearMedications() async {
+    try {
+      await HiveManager.clear<Medication>(BoxNames.medications);
+      state = [];
+      return OperationStatus.success;
+    } catch (e) {
+      print('Error clearing medications: $e');
+      return OperationStatus.failure;
+    }
   }
 }
