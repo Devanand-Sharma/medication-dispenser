2f9fbe5916eecb7d9e35a41e75bdde5585c0cff1
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-09-20 22:04:08 -0400

    Added Scheduling Compatibility with Other MedicationFrequency
    - Made end date optional
    - Support for all frequency types (excluding Cyclic)
    - Cleaned up medication_form.dart
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- lib/models/medication_frequency.dart
+++ lib/models/medication_frequency.dart
@@ -5,7 +5,8 @@ enum MedicationFrequency {
   everyXDays,
   everyXWeeks,
   everyXMonths,
-  onARecurringCycle,
+  // TODO: Add support for onARecurringCycle
+  // onARecurringCycle,
   onlyAsNeeded,
 }
 
@@ -24,10 +25,93 @@ extension MedicationFrequencyExtension on MedicationFrequency {
         return 'Every X Weeks';
       case MedicationFrequency.everyXMonths:
         return 'Every X Months';
-      case MedicationFrequency.onARecurringCycle:
-        return 'On a Recurring Cycle';
+      // case MedicationFrequency.onARecurringCycle:
+      //   return 'On a Recurring Cycle';
       case MedicationFrequency.onlyAsNeeded:
         return 'Only as Needed';
     }
+  }
+}
+
+bool isFrequencyCount(MedicationFrequency? frequency) {
+  switch (frequency) {
+    case MedicationFrequency.xTimesADay:
+      return true;
+    case MedicationFrequency.everyXHours:
+      return true;
+    case MedicationFrequency.everyXDays:
+      return true;
+    case MedicationFrequency.everyXWeeks:
+      return true;
+    case MedicationFrequency.everyXMonths:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool isEveryX(MedicationFrequency? frequency) {
+  switch (frequency) {
+    case MedicationFrequency.everyXHours:
+      return true;
+    case MedicationFrequency.everyXDays:
+      return true;
+    case MedicationFrequency.everyXWeeks:
+      return true;
+    case MedicationFrequency.everyXMonths:
+      return true;
+    default:
+      return false;
+  }
+}
+
+int? setMedicationFrequencyCount(MedicationFrequency? value) {
+  switch (value) {
+    case MedicationFrequency.xTimesADay:
+      return 2;
+    case MedicationFrequency.everyXHours:
+      return 2;
+    case MedicationFrequency.everyXDays:
+      return 2;
+    case MedicationFrequency.everyXWeeks:
+      return 1;
+    case MedicationFrequency.everyXMonths:
+      return 1;
+    default:
+      return null;
+  }
+}
+
+int setMinMedicationFrequencyCount(MedicationFrequency? value) {
+  switch (value) {
+    case MedicationFrequency.xTimesADay:
+      return 2;
+    case MedicationFrequency.everyXHours:
+      return 1;
+    case MedicationFrequency.everyXDays:
+      return 2;
+    case MedicationFrequency.everyXWeeks:
+      return 1;
+    case MedicationFrequency.everyXMonths:
+      return 1;
+    default:
+      return 1;
+  }
+}
+
+int setMaxMedicationFrequencyCount(MedicationFrequency? value) {
+  switch (value) {
+    case MedicationFrequency.xTimesADay:
+      return 6;
+    case MedicationFrequency.everyXHours:
+      return 1;
+    case MedicationFrequency.everyXDays:
+      return 60;
+    case MedicationFrequency.everyXWeeks:
+      return 12;
+    case MedicationFrequency.everyXMonths:
+      return 12;
+    default:
+      return 1;
   }
 }
